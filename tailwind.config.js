fabfb12a4acb8f3b068232841bd21af4dcd18f91
Adrian <aantosiak@icloud.com>
2023-06-29 16:32:18 +0200

    first

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,33 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        bruss: '#8bb63b',
+      },
     },
   },
-  plugins: [],
+  plugins: [require('prettier-plugin-tailwindcss')],
 }
+
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   plugins: [require('prettier-plugin-tailwindcss')],
+//   content: [
+//     './pages/**/*.{js,ts,jsx,tsx,mdx}',
+//     './components/**/*.{js,ts,jsx,tsx,mdx}',
+//     './app/**/*.{js,ts,jsx,tsx,mdx}',
+//   ],
+//   theme: {
+//     fontFamily: {
+//       sans: [
+//         'Inter var, sans-serif',
+//         { fontFeatureSettings: '"cv11", "ss01"' },
+//       ],
+//     },
+//     extend: {
+//       colors: {
+//         bruss: '#8bb63b',
+//       },
+//     },
+//   },
+// }
