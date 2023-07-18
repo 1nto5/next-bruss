cc487594b5bfd665b06153f58e9f7365bdc55f07
Adrian <aantosiak@icloud.com>
2023-07-18 14:09:03 +0200

    comments

--- lib/mongo/connector.ts
+++ lib/mongo/connector.ts
@@ -4,17 +4,17 @@ export async function connectToMongo(
   collectionName: string
 ): Promise<Collection> {
   const client = new MongoClient(process.env.MONGO_URI as string)
-  const dbName = 'next_bruss' // Replace with your DB name
+  const dbName = 'next_bruss'
 
   try {
     await client.connect()
-    console.log('Connected successfully to MongoDB server') // success log
+    console.log('Connected successfully to MongoDB server')
 
     const db = client.db(dbName)
     const collection = db.collection(collectionName)
-    return collection // <- return the collection here
+    return collection
   } catch (e) {
     console.error('Failed to connect to MongoDB', e)
-    throw e // <- also good to throw the error so the calling function can handle it
+    throw e
   }
 }
