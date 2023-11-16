587f1ffded3901f79f0a0960cbee74e5688b17fa
Adrian <aantosiak@icloud.com>
2023-11-16 14:40:54 +0100

    config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: {
-      allowedOrigins: ['http://localhost', 'http://10.27.10.127'],
-    },
+    // serverActions: true,
   },
 };
 
