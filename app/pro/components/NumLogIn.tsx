2fe982964f0d976b2e789654ab0e9d86dbfa5b24
Adrian <aantosiak@icloud.com>
2023-11-20 11:35:31 +0100

    fix num log

--- app/pro/components/NumLogIn.tsx
+++ app/pro/components/NumLogIn.tsx
@@ -14,7 +14,7 @@ type NumberButtonProps = {
 
 const NumberButton: React.FC<NumberButtonProps> = ({ onClick, value }) => (
   <button
-    className='m-4 inline-block h-20 w-20 rounded bg-slate-100 text-center text-4xl text-slate-900 shadow-sm hover:bg-slate-200 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600'
+    className='m-2 inline-block h-24 w-24 rounded bg-slate-100 text-center text-4xl text-slate-900 shadow-sm hover:bg-slate-200 dark:bg-slate-700 dark:text-slate-100 dark:hover:bg-slate-600'
     onClick={onClick}
     type='button'
   >
@@ -61,7 +61,7 @@ const NumLogIn = () => {
       onSubmit={handleLogin}
     >
       <input
-        className='font mt-4 h-20 w-3/12 rounded-lg bg-slate-100 text-center text-4xl text-slate-900 shadow-sm outline-none dark:bg-slate-700 dark:text-slate-100'
+        className='font mt-4 h-24 w-3/12 rounded-lg bg-slate-100 text-center text-4xl text-slate-900 shadow-sm outline-none dark:bg-slate-700 dark:text-slate-100'
         type='text'
         value={personalNumber}
         onChange={(e) => setPersonalNumber(e.target.value)}
@@ -86,7 +86,7 @@ const NumLogIn = () => {
         </div>
         <div>
           <button
-            className='m-4 inline-block h-20 w-20 rounded bg-red-500 text-center text-4xl text-slate-50 shadow-sm hover:bg-red-700 dark:bg-red-800 dark:text-slate-100 dark:hover:bg-red-600'
+            className='m-2 inline-block h-24 w-24 rounded bg-red-500 text-center text-4xl text-slate-50 shadow-sm hover:bg-red-700 dark:bg-red-800 dark:text-slate-100 dark:hover:bg-red-600'
             onClick={() => handleClickClear()}
             type='button'
           >
@@ -94,7 +94,7 @@ const NumLogIn = () => {
           </button>
           <NumberButton onClick={() => handleClickNumber(0)} value={0} />
           <button
-            className='m-4 h-20 w-20 rounded bg-bruss text-center text-4xl text-slate-50 shadow-sm hover:bg-green-400 dark:bg-green-800 dark:text-slate-100 dark:hover:bg-green-700'
+            className='m-2 h-24 w-24 rounded bg-bruss text-center text-4xl text-slate-50 shadow-sm hover:bg-green-700 dark:bg-green-800 dark:text-slate-100 dark:hover:bg-green-700'
             type='submit'
           >
             OK
