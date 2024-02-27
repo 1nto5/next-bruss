210460798954d308b57aff27dca7094877be61ab
Adrian <aantosiak@icloud.com>
2024-02-27 14:05:35 +0100

    pm2

--- pm2.config.js
+++ pm2.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'next-bruss',
       script: './node_modules/next/dist/bin/next',
       args: 'start',
-      interpreter: 'bun',
+      //   interpreter: 'bun',
       env: {
         PORT: 3000,
       },
