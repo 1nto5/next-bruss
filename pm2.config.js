855fef04b51f3c3ee02acb9a754d2a6d79aa1329
Adrian <aantosiak@icloud.com>
2024-02-27 14:00:59 +0100

    pm2

--- pm2.config.js
+++ pm2.config.js
@@ -5,10 +5,6 @@ module.exports = {
       script: './node_modules/next/dist/bin/next',
       args: 'start',
       interpreter: 'bun',
-      env: {
-        PORT: 3000,
-        NODE_ENV: 'production',
-      },
     },
   ],
 };
