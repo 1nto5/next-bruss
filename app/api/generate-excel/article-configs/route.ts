56d52811578f23da4f5503b8022d521997accf7a
Adrian <aantosiak@icloud.com>
2024-03-04 15:58:20 +0100

    articles config api for fetch in export data

---
+++ app/api/generate-excel/article-configs/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse, type NextRequest } from 'next/server';
+import { dbc } from '@/lib/mongo';
+
+export async function GET(req: NextRequest) {
+  const collection = await dbc('articles_config');
+  const configs = await collection.find().toArray();
+  // console.log('pobieram configi', configs);
+  if (!configs) {
+    return new NextResponse(JSON.stringify({ message: 'configs not found' }));
+  }
+  return new NextResponse(JSON.stringify(configs));
+}
