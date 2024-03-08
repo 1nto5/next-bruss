a6aa5b2a0dcc870dd57afddceb29d31237fbc33a
Adrian <aantosiak@icloud.com>
2024-03-08 11:52:31 +0100

    todos

--- TODOS.js
+++ TODOS.js
@@ -1,3 +1,7 @@
 /*
+TODO: connectin with external API for BRI
 TODO: EOL136153 miration to new version
+TODO: accept two variant article name in hydra label (SAP: 10.040040.00)
+TODO: 50 last scans
+TODO: how much scans on shift for operator
 */
