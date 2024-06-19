d3a23a96823c89223f31b8d199ab5118f8cb186f
Adrian <aantosiak@icloud.com>
2024-06-19 11:54:46 +0200

    fix logo flickering

--- app/(users)/[lang]/components/Logo.tsx
+++ app/(users)/[lang]/components/Logo.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import logo from '@/public/logo.png';
 
 type LogoProps = {
   logoStyles?: string;
@@ -6,13 +7,26 @@ type LogoProps = {
 
 export default function Logo({ logoStyles }: LogoProps) {
   return (
+    // <Image
+    //   className={logoStyles ?? ''}
+    //   src='/logo.png'
+    //   width={80}
+    //   height={100}
+    //   alt='BRUSS'
+    //   // priority={false}
+    // />
     <Image
-      className={logoStyles ?? ''}
-      src='/logo.png'
-      width={80}
-      height={100}
+      src={logo}
+      // You don't need to specify
+      //either height nor width but
+      // you should if you wan't to
+      // change the size
+      // height={100}
+      width={100}
+      // Optional blur-up
+      // while loading
+      // placeholder='blur'
       alt='BRUSS'
-      // priority={false}
     />
   );
 }
