afe409dcdcfa0fa431409bc1691c8c9aedd762d6
Adrian <aantosiak@icloud.com>
2024-06-21 12:41:53 +0200

    todos

--- TODOS.js
+++ TODOS.js
@@ -1,7 +1,5 @@
 /*
 TODO: EOL136153 miration to new version
-TODO: accept two variant article name in hydra label (SAP: 10.040040.00) - article config done
 TODO: 50 last scans on scan page
 TODO: how much scans on shift for operator
-TODO: reset the counter status - option for TL's
 */
