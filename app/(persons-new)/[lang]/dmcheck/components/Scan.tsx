be84c2dca08f16d6f50dea2ea3afbd22aa4d1b4e
Adrian <aantosiak@icloud.com>
2024-06-26 13:49:57 +0200

    nok sounds

--- app/(persons-new)/[lang]/dmcheck/components/Scan.tsx
+++ app/(persons-new)/[lang]/dmcheck/components/Scan.tsx
@@ -35,68 +35,90 @@ export function Scan({
       'Invalid volume value. Please provide a value between 0.0 and 1.0.',
     );
   }
-  const [playSuccess, { sound: successSound }] = useSound('/success.wav', {
+  const [playOk, { sound: okSound }] = useSound('/ok.wav', {
     volume: volume,
   });
+
+  const [playNok, { sound: nokSound }] = useSound('/nok.wav', {
+    volume: volume,
+  });
+
   useEffect(() => {
     switch (state?.message) {
       case 'dmc saved':
-        playSuccess();
+        playOk();
         toast.success(cDict.toast.dmcSaved);
         break;
       case 'batch saved':
-        playSuccess();
+        playOk();
         toast.success(cDict.toast.batchSaved);
         break;
       case 'dmc exists':
+        playNok();
         toast.error(cDict.toast.dmcExists);
         break;
       case 'batch exists':
+        playNok();
         toast.error(cDict.toast.batchExists);
         break;
       case 'dmc not valid':
+        playNok();
         toast.error(cDict.toast.dmcNotValid);
         break;
       case 'qr not valid':
+        playNok();
         toast.error(cDict.toast.qrNotValid);
         break;
       case 'article not found' || 'wrong article config id':
+        playNok();
         toast.error(cDict.toast.articleNotFound);
         break;
       case 'ford date not valid':
+        playNok();
         toast.error(cDict.toast.fordDateNotValid);
         break;
       case 'bmw date not valid':
+        playNok();
         toast.error(cDict.toast.bmwDateNotValid);
         break;
       case 'qr wrong article':
+        playNok();
         toast.error(cDict.toast.qrWrongArticle);
         break;
       case 'qr wrong quantity':
+        playNok();
         toast.error(cDict.toast.qrWrongQuantity);
         break;
       case 'qr wrong process':
+        playNok();
         toast.error(cDict.toast.qrWrongProcess);
         break;
       case 'saving error':
+        playNok();
         toast.error(cDict.toast.unknownSavingError);
         break;
       case '40040 nok':
+        playNok();
         toast.error(cDict.toast.nok);
         break;
       case 'smart not found':
+        playNok();
         toast.error('Nie znaleziono w bazie SMART!');
         break;
       case 'smart unknown':
+        playNok();
         toast.error('SMART: Część nieprzeprocesowana na wszystkich stacjach!');
         break;
       case 'smart nok':
+        playNok();
         toast.error('SMART: Część otrzymała status NOK na jednej ze stacji!'); // can we determine which station in the future?
         break;
       case 'smart pattern':
+        playNok();
         toast.error('SMART: Część jest wzornikiem!');
         break;
       case 'smart fetch error':
+        playNok();
         toast.error('Problem połączenia z bazą SMART!');
         break;
       default:
