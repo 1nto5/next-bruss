97051b4a2bba22200eec57207b3b6f7a6600cc38
Adrian <aantosiak@icloud.com>
2024-07-29 15:33:42 +0200

    TODOS

--- TODOS.js
+++ TODOS.js
@@ -1,5 +1,5 @@
 /*
-TODO: EOL136153 miration to new version
+TODO: EOL136153 migration to new version
 TODO: 50 last scans on scan page (or / and list 3 sifts)
 TODO: how much scans on shift for operator
 TODO: MS365 login
