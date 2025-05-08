33fc2ca53ebd24f254c7386765cd6c3c2f3cfc36
1nto5 <aantosiak@icloud.com>
2025-05-08 16:18:43 +0200

    fix: include query string in locale redirect for middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
 import { i18n } from '@/i18n.config';
 
@@ -20,6 +20,7 @@ function getLocale(request: NextRequest): string | undefined {
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
+  const search = request.nextUrl.search; // Get the query string
 
   // // `/_next/` and `/api/` are ignored by the watcher, but we need to ignore files in `public` manually.
   // // If you have one
@@ -39,11 +40,20 @@ export function middleware(request: NextRequest) {
   );
 
   // Redirect if there is no locale
+  // if (pathnameIsMissingLocale) {
+  //   const locale = getLocale(request);
+  //   return NextResponse.redirect(
+  //     new URL(
+  //       `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
+  //       request.url,
+  //     ),
+  //   );
+  // }
   if (pathnameIsMissingLocale) {
     const locale = getLocale(request);
     return NextResponse.redirect(
       new URL(
-        `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`,
+        `/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}${search}`,
         request.url,
       ),
     );
