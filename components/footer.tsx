9fea091b6b2bc2be94297e0f5313c56b262dd62f
1nto5 <aantosiak@icloud.com>
2025-05-13 06:53:34 +0200

    fix: update footer to abbreviate name for consistency

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className={`border-t px-2 py-1`}>
       <div className='text-muted-foreground mx-auto max-w-7xl text-right text-xs'>
-        Adrian Antosiak 2025
+        A. Antosiak 2025
       </div>
     </footer>
   );
