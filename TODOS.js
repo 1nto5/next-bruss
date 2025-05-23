b7ef4c719803932b474f1ba3fe8c9afb1b0fbd00
1nto5 <aantosiak@icloud.com>
2025-05-23 16:06:12 +0200

    todos

--- TODOS.js
+++ TODOS.js
@@ -1,4 +1,11 @@
 /*
+curl -X POST http://10.27.202.15:5000/run-automation \
+  -H "Authorization: Bearer ad126ea0-a92b-4ce5-9c77-e8d9024d9867" \
+  -H "Content-Type: application/json" \
+  -d '{"identifier":"95509"}';
+
+
+
 
 FIXME: shadcn calendar is not compatible with: "react-day-picker": "^9", which is used in the project for datetime-picker
 FIXME: duplicate mongodb errors in pm2 logs
@@ -14,8 +21,14 @@ TODO: DMCheck v3 - zustand + react-query
 TODO: users admin rebuild
 TODO: all dialogs formatting as in failures lv
 
-
-
+TODO: DMCheck - HYDRA label printing automation
+- Add button to print HYDRA label when box is full in DMCheck application
+- Check articleConfig for printHydraLabelAipIp property
+- Use the IP from articleConfig to call the automation API endpoint
+- Include proper loading/status indication while print job is processing
+- Implement error handling for failed API calls
+- Use existing authorization token: ad126ea0-a92b-4ce5-9c77-e8d9024d9867
+- API endpoint format: http://{printHydraLabelAipIp}:5000/run-automation
 
 TODO: odchylenia: 
 Backend
