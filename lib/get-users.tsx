f801b7cda6d67a3d4690052c46f41766ed121c0d
1nto5 <aantosiak@icloud.com>
2025-06-26 14:44:34 +0200

    Add getUsers function to fetch user data from API

---
+++ lib/get-users.tsx
@@ -0,0 +1,16 @@
+import { UsersListType } from './types/user';
+
+export async function getUsers(): Promise<UsersListType> {
+  const res = await fetch(`${process.env.API}/users`, {
+    next: { revalidate: 60 * 60 * 8, tags: ['users'] },
+  });
+
+  if (!res.ok) {
+    const json = await res.json();
+    throw new Error(
+      `getUsers error:  ${res.status}  ${res.statusText} ${json.error}`,
+    );
+  }
+  const data = await res.json();
+  return data;
+}
