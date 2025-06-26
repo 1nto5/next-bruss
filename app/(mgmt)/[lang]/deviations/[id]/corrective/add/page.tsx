7fb24816008aa9107e2a51ceec148d1df389c998
1nto5 <aantosiak@icloud.com>
2025-06-26 14:44:09 +0200

    Refactor AddDeviationPage: remove redundant getUsers function and streamline user fetching

--- app/(mgmt)/[lang]/deviations/[id]/corrective/add/page.tsx
+++ app/(mgmt)/[lang]/deviations/[id]/corrective/add/page.tsx
@@ -1,22 +1,7 @@
 import { Locale } from '@/i18n.config';
 // import { getDictionary } from '@/lib/dictionary';
-import { UsersListType } from '@/lib/types/user';
+import { getUsers } from '@/lib/get-users';
 import AddCorrectiveActionForm from '../../../components/add-corrective-action-form';
-
-async function getUsers(): Promise<UsersListType> {
-  const res = await fetch(`${process.env.API}/users`, {
-    next: { revalidate: 60 * 60 * 24, tags: ['users'] },
-  });
-
-  if (!res.ok) {
-    const json = await res.json();
-    throw new Error(
-      `getUsersEmail error:  ${res.status}  ${res.statusText} ${json.error}`,
-    );
-  }
-  const data = await res.json();
-  return data;
-}
 
 export default async function AddDeviationPage(props: {
   params: Promise<{ lang: Locale; id: string }>;
