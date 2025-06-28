3a69b3e2ef43621d99051a3b29f8ec0af771d5bc
1nto5 <aantosiak@icloud.com>
2025-06-28 11:11:10 +0200

    Refactor quantity validation in UpdatePositionSchema to support both string and number types

--- app/(mgmt)/[lang]/inw-2/zatwierdz/lib/zod.ts
+++ app/(mgmt)/[lang]/inw-2/zatwierdz/lib/zod.ts
@@ -3,12 +3,17 @@ import * as z from 'zod';
 export const UpdatePositionSchema = z.object({
   articleNumber: z.string(),
   quantity: z
-    .string()
-    .min(1, 'Ilość jest wymagana')
-    .refine((val) => !isNaN(Number(val)), {
-      message: 'Ilość musi być liczbą',
-    })
-    .transform((val) => Number(val))
+    .union([z.string(), z.number()])
+    .refine(
+      (val) => {
+        const num = typeof val === 'number' ? val : Number(val);
+        return !isNaN(num);
+      },
+      {
+        message: 'Ilość musi być liczbą',
+      },
+    )
+    .transform((val) => (typeof val === 'number' ? val : Number(val)))
     .refine((val) => val >= 0, {
       message: 'Ilość nie może być ujemna',
     }),
