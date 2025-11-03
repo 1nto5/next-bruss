6a5a0156d0ccdcc65fa30483fc4d861155e6793e
1nto5 <aantosiak@icloud.com>
2025-11-03 11:28:06 +0100

    update deps, migrate prettier to mjs, rm tailwind test

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,10 +1,8 @@
-module.exports = {
-  tailwindConfig: './tailwind.config.js',
+export default {
   bracketSpacing: true,
   printWidth: 80,
   singleQuote: true,
   quoteProps: 'consistent',
   jsxSingleQuote: true,
   trailingComma: 'all',
-  plugins: ['prettier-plugin-tailwindcss'],
 };
