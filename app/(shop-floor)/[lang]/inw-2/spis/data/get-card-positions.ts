594253119e059d3e92d2bfddbb67b2395d39429d
1nto5 <aantosiak@icloud.com>
2025-11-06 10:36:56 +0100

    remove shopflor - migration

--- app/(shop-floor)/[lang]/inw-2/spis/data/get-card-positions.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { fetchCardPositions } from '../actions';
-
-export function useGetCardPositions(persons: string[], card: number) {
-  return useQuery({
-    queryFn: async () => fetchCardPositions(persons, card),
-    queryKey: ['positions'],
-  });
-}
--- app/(shop-floor)/[lang]/inw-2/spis/data/get-cards.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query';
-import { fetchCards } from '../actions';
-
-export function useGetCards(persons: string[]) {
-  return useQuery({
-    queryFn: async () => fetchCards(persons),
-    queryKey: ['cards'],
-  });
-}
